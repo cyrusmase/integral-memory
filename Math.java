68396eceecc54ae3764fe02e7c4aa0e5a2ecd068
cyrusmase <cyrusmase@gmail.com>
2024-03-14 17:09:30 -0700

    initial file upload - Cyrus M

---
+++ Math.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Math{
+    public static void main(String[] args){
+        Scanner c = new Scanner(System.in);
+        int choice;
+        System.out.println("Select from the menu above: ");
+        choice = c.nextInt();
+        System.out.println("Input: "+choice);
+    }
+}
