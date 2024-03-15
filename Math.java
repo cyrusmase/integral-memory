dca75f38c06d73ba7051a85e7066dab44938c0ba
cyrusmase <cyrusmase@gmail.com>
2024-03-14 17:32:03 -0700

    added specific logic for change X equation - CM

--- Math.java
+++ Math.java
@@ -1,10 +1,53 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Math{
     public static void main(String[] args){
+        System.out.print("------------------\n(1) Find Change X\n------------------\n");
+        System.out.println("Select from the menu above: ");
+    start(); 
+    }
+    public static void start(){
         Scanner c = new Scanner(System.in);
         int choice;
-        System.out.println("Select from the menu above: ");
         choice = c.nextInt();
-        System.out.println("Input: "+choice);
+       // System.out.println("Input: "+choice);
+        switch(choice){
+            case 1:
+            findChangeX();
+            break;
+            case 2:
+
+            break;
+            case 3:
+
+            break;
+            case 4:
+
+            break;
+            case 5:
+            
+            break;
+        }
+        c.close();
+    }
+    public static void findChangeX(){
+        try{
+        int a,b,n;
+        String answer;
+        Scanner x = new Scanner(System.in);
+        System.out.print("enter value of b: ");
+        b = x.nextInt();
+        System.out.print("enter value of a: ");
+        a = x.nextInt();
+        System.out.print("enter value of n: ");
+        n = x.nextInt();
+        answer = ((b-a)+"/"+n);
+        x.close();
+        System.out.println("Answer: "+ answer);
+        }catch(InputMismatchException e){
+            System.out.println("Please enter a valid integer!");
+            findChangeX();
+        }
+
     }
 }
