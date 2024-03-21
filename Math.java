2950b55c18b28a405378fb86ac29c2b8ca0bc81d
cyrusmase <cyrusmase@gmail.com>
2024-03-21 15:31:09 -0700

    broken end - NoSucheElementE

--- Math.java
+++ Math.java
@@ -2,16 +2,13 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Math{
-
-    public static void main(String[] args){
-    menu();
-    start(); 
-    }
     public static void menu(){
-        System.out.print("------------------\n(1) Find Change X\n------------------\n(2) NI\n(3) NI\n(4) NI\n(5) NI\n");
+        System.out.print("------------------\n(1) Find Change X\n(2) NI\n(3) NI\n(4) NI\n(5) Exit\n------------------\n");
         System.out.print("(*) Select from the menu above: ");
     }
     public static void start(){
+        boolean end = false;
+        while(end != true){
         Scanner c = new Scanner(System.in);
         try{
         int choice;
@@ -31,20 +28,24 @@ public class Math{
 
             break;
             case 5:
-            
+            System.out.println("Closing...");
+            end = true;
             break;
         }
-        c.close();
+   
     }catch(InputMismatchException e){
         System.out.println("\n(x) Please enter a VALID choice from the list!\n");
         menu();
         start();
     }
+    System.out.println("Closed!");
+    c.close();
+}
 }
     public static void findChangeX(){
         try{
         int a,b,n;
-        String answer;
+        String answer,endpoint;
         double dec_answer;
         Scanner x = new Scanner(System.in);
         System.out.print("  Enter value of b: ");
@@ -53,11 +54,25 @@ public class Math{
         a = x.nextInt();
         System.out.print("  Enter value of n: ");
         n = x.nextInt();
-        answer = ((b-a)+"/"+n);
-        dec_answer = ((b-a)/n);
+        System.out.print("    Enter which endpoint is being searched for [left, middle, right]: ");
+        endpoint = x.next();
+        if(endpoint.equalsIgnoreCase("left")){
+            System.out.println("NI");
+            start();
+        }
+        if(endpoint.equalsIgnoreCase("middle")){
+            System.out.println("The equation used is as follows: "+"("+b+"-"+a+")/"+n);
+            answer = ((b-a)+"/"+n);
+            dec_answer = ((b-a)/n);
+            System.out.println("Answer in fraction form: "+ answer);
+            System.out.println("Answer in decimal form: "+ dec_answer);
+        }
+        if(endpoint.equalsIgnoreCase("right")){
+            System.out.println("NI");
+            start();
+        }
+
         x.close();
-        System.out.println("Answer in fraction form: "+ answer);
-        System.out.println("Answer in decimal form: "+ dec_answer);
         System.out.println("Function complete!\n");
         }catch(InputMismatchException e){
             System.out.println("(x) Please enter a valid integer!");
@@ -65,4 +80,8 @@ public class Math{
         }
 
     }
+    public static void main(String[] args){
+        menu();
+        start(); 
+        }
 }
