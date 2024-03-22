a1a8a0a4986d2ccfc7a96727779c02afefe00098
cyrusmase <cyrusmase@gmail.com>
2024-03-22 14:10:24 -0700

    MK_fix- thank you!<3

--- Math.java
+++ Math.java
@@ -1,87 +1,119 @@
-import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
-public class Math{
 
-    public static void main(String[] args){
-    menu();
-    start(); 
-    }
-    public static void menu(){
-        System.out.print("------------------\n(1) Find Change X\n(2) NI\n(3) NI\n(4) NI\n(5) Exit\n------------------\n");
-        System.out.print("(*) Select from the menu above: ");
-    }
-    public static void start(){
-        boolean end = false;
-        while(end != true){
-        Scanner c = new Scanner(System.in);
-        try{
-        int choice;
-        choice = c.nextInt();
-       // System.out.println("Input: "+choice);
-        switch(choice){
-            case 1:
-            findChangeX();
-            break;
-            case 2:
+public class Main {
 
-            break;
-            case 3:
+    private static final Scanner scanner = new Scanner(System.in);
 
-            break;
-            case 4:
-
-            break;
-            case 5:
-            System.out.println("Closing...");
-            end = true;
-            break;
-        }
-        c.close();
-    }catch(InputMismatchException e){
-        System.out.println("\n(x) Please enter a VALID choice from the list!\n");
+    public static void main(String[] args) {
         menu();
         start();
     }
-    System.out.println("Closed!");
-}
-}
-    public static void findChangeX(){
-        try{
-        int a,b,n;
-        String answer,endpoint;
-        double dec_answer;
-        Scanner x = new Scanner(System.in);
-        System.out.print("  Enter value of b: ");
-        b = x.nextInt();
-        System.out.print("  Enter value of a: ");
-        a = x.nextInt();
-        System.out.print("  Enter value of n: ");
-        n = x.nextInt();
-        System.out.print("    Enter which endpoint is being searched for [left, middle, right]: ");
-        endpoint = x.next();
-        if(endpoint.equalsIgnoreCase("left")){
-            System.out.println("NI");
-            start();
-        }
-        if(endpoint.equalsIgnoreCase("middle")){
-            System.out.println("The equation used is as follows: "+"("+b+"-"+a+")/"+n);
-            answer = ((b-a)+"/"+n);
-            dec_answer = ((b-a)/n);
-            System.out.println("Answer in fraction form: "+ answer);
-            System.out.println("Answer in decimal form: "+ dec_answer);
-        }
-        if(endpoint.equalsIgnoreCase("right")){
-            System.out.println("NI");
-            start();
-        }
 
-        x.close();
-        System.out.println("Function complete!\n");
-        }catch(InputMismatchException e){
+    public static void menu() {
+        System.out.print(
+            "------------------\n" +
+            "(1) Find Change X\n" +
+            "(2) NI\n" +
+            "(3) NI\n" +
+            "(4) NI\n" +
+            "(5) Exit\n" +
+            "------------------\n");
+        System.out.print("(*) Select from the menu above: ");
+    }
+
+    public static void start() {
+        boolean end = false;
+        while (!end ) { // clean code
+                int choice = readInt();
+//                System.out.println("Input: "+choice);
+                switch (choice) {
+                    case 1:
+                        findChangeX();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+                        System.out.println("Closing...");
+                        end = true;
+                        break;
+                    default:
+                        // print error
+                }
+        }
+        System.out.println("Closed!");
+        scanner.close();
+    }
+
+    public static void findChangeX() {
+        try {
+            int a, b, n;
+            String answer, endpoint;
+            double dec_answer;
+            System.out.print("  Enter value of b: ");
+            b = readInt();
+            System.out.print("  Enter value of a: ");
+            a = readInt();
+            System.out.print("  Enter value of n: ");
+            n = readInt();
+
+            System.out.print("    Enter which endpoint is being searched for [left, middle, right]: ");
+            endpoint = readString();
+            if (endpoint.equalsIgnoreCase("left") || endpoint.equalsIgnoreCase("l")) {
+                System.out.println("NI");
+                start();
+            }
+            if (endpoint.equalsIgnoreCase("middle")) {
+                System.out.println("The equation used is as follows: " + "(" + b + "-" + a + ")/" + n);
+                answer = ((b - a) + "/" + n);
+                dec_answer = ((b - a) / (double) n);
+                System.out.println("Answer in fraction form: " + answer);
+                System.out.println("Answer in decimal form: " + dec_answer);
+            }
+            if (endpoint.equalsIgnoreCase("right")) {
+                System.out.println("NI");
+                start();
+            }
+
+            System.out.println("Function complete!\n");
+        } catch (InputMismatchException e) {
             System.out.println("(x) Please enter a valid integer!");
             findChangeX();
         }
+    }
 
+    private static int readInt() {
+        while (true) {
+            try {
+                int val = scanner.nextInt();
+                return val;
+            } catch (InputMismatchException e) {
+                System.err.println("Input must be a number");
+                scanner.next();
+            } catch (NoSuchElementException | IllegalStateException e) {
+                System.err.println("Ran out of input");
+                System.exit(1);
+            }
+        }
+    }
+
+    private static String readString() {
+        while (true) {
+            try {
+                String val = scanner.next();
+                return val;
+            } catch (NoSuchElementException | IllegalStateException e) {
+                System.err.println("Ran out of input");
+                System.exit(1);
+            }
+        }
     }
 }
